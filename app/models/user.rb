fe34719085884f653a4367c20cb28d2fea13d238
LeQuangMinhSun <le.quang.minh@sun-asterisk.com>
2024-07-25 16:42:21 +0700

    chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
+  USER_PARAMS = %i(name email password password_confirmation).freeze
   VALID_EMAIL_REGEX = Regexp.new Settings.email.valid_regex
 
   before_save :downcase_email
 
   validates :name, presence: true, length: {maximum: Settings.name.max_length}
-  validates :email, presence: true, length: {maximum: Settings.email.max_length},
-    format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+  validates :email, presence: true,
+            length: {maximum: Settings.email.max_length},
+            format: {with: VALID_EMAIL_REGEX}, uniqueness: true
 
   has_secure_password
 
