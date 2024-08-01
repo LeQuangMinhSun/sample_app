ac5625d2142b2004ba9f414bc7c4515493b9c54f
LeQuangMinhSun <le.quang.minh@sun-asterisk.com>
2024-08-01 10:29:43 +0700

    chapter 13

---
+++ app/models/micropost.rb
@@ -0,0 +1,17 @@
+class Micropost < ApplicationRecord
+  MICROPOST_PARAMS = %i(content image).freeze
+  belongs_to :user
+  has_one_attached :image
+
+  scope :newest, ->{order(created_at: :desc)}
+  validates :content, presence: true, length: {maximum: Settings.digit_140}
+  validates :image, content_type: {in: Settings.image.type,
+                                   message: I18n.t("message.img_format")},
+            size: {less_than: Settings.image.max_size.megabytes,
+                   message: I18n.t("message.img_format")}
+
+  has_one_attached :image do |attachable|
+    attachable.variant :display, resize_to_limit: [Settings.image.max_height,
+                                                   Settings.image.max_width]
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,8 @@ Bundler.require(*Rails.groups)
 module RailsTutorial
   class Application < Rails::Application
     config.load_defaults 7.0
-    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+    config.active_storage.variant_processor = :mini_magick
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml,js}")]
     config.i18n.available_locales = [:vi, :en]
     config.i18n.default_locale = :en
   end
